e87f723129262f32317281904698e0107d388c36
ajcendejas15 <ajcendejas15@gmail.com>
2014-05-27 10:42:59 -0600

    Initial Commit

---
+++ Homework Assignment 3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Homework Assignment 3
+//
+//  Created by AJ Cendejas on 5/27/14.
+//  Copyright (c) 2014 AJ Cendejas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
